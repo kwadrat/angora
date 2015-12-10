058b9ed647c7f0577f9d0b225cd8d2185a395d67
Piotr Kasprzyk <ciri@ciri.pl>
2015-12-10 20:23:03 +0100

    Initial commit

---
+++ angora.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+rows = [
+    [3],
+    [1,3],
+    [9],
+    [5],
+    [2],
+    [1],
+    [2],
+    [3],
+    [8],
+    [9],
+    [9],
+    [12],
+    [14],
+    [5],
+    [1,1],
+    [1,1],
+    [5],
+    [2,1],
+    [1],
+    [2],
+    ]
+
+cols = [
+    [0],
+    [0],
+    [0],
+    [1],
+    [1],
+    [1],
+    [1],
+    [3,3],
+    [1,2,6],
+    [13,1],
+    [5,5,1],
+    [3,6,1],
+    [6,1,1],
+    [12],
+    [6,1],
+    [8],
+    [3],
+    [2],
+    [2],
+    [2],
+    [1],
+    [1],
+    [1],
+    [0],
+    [0],
+    ]
+
+assert sum(map(sum, rows)) == sum(map(sum, cols))
+
+all_lines = open('plansza.txt', 'rb').read().splitlines()
+
+def decode(seq):
+    return 0
+
+class TestGazowychKolumn(unittest.TestCase):
+    def test_column_hiding(self):
+        '''
+        TestGazowychKolumn:
+        '''
+        self.assertEqual(decode(''), 0)
