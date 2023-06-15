801dc01d5f9d4a502a800813afb71d085011c761
Piotr Kasprzyk <ciri@ciri.pl>
2023-06-15 15:37:00 +0200

    Separate test core to ts_code

---
+++ ts_code.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import unittest
+
+import im_chisel
+import eg_bag
+import lb_cnst
+
+
+fast_test_ls = [
+    eg_bag.TestBagFunctions,
+    lb_cnst.TestConstants,
+    im_chisel.TestChiselTool,
+    ]
+
+
+def summary_status(suite):
+    text_test_result = unittest.TextTestRunner().run(suite)
+    return not not (text_test_result.failures or text_test_result.errors)
+
+
+def perform_tests():
+    suite = unittest.TestSuite()
+    for one_test in fast_test_ls:
+        suite.addTest(unittest.makeSuite(one_test))
+    return summary_status(suite)
