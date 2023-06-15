9c9fafb2bab55d31508dae3c8479ecbceca2007b
Piotr Kasprzyk <ciri@ciri.pl>
2023-06-15 14:40:44 +0200

    Move rows and columns to fx_area

--- angora.py
+++ angora.py
@@ -5,75 +5,12 @@
 python -m unittest angora; red_green_bar.py $? $COLUMNS
 '''
 
+import fx_area
+
 import sys
 import unittest
 import itertools
 
-rows = '''\
-2 1 3 7
-8 3 1 1 3 3
-9 6 1 5 3
-9 4 4 1
-1 4 4 3 1
-3 1 1 4
-5 3 5
-1 4 1 6 6
-6 8 6 2
-5 3 4 1 4
-1 6 5 3 4
-1 2 10 4
-3 1 11 3
-2 1 2 11
-3 9
-8 3 1
-3 1 2 2 1
-7 1 1 3
-1 9 1 1 4 1 1
-10 2 9
-1 8 13
-2 2 2 1 5 3
-2 3 1 1 3 2 3
-2 1 1 1 1 3 3
-1 6 4 1 5
-5 4 3 5
-3 6 3
-2 1 1 1
-1 2 1
-1 3 2 2 1 2
-'''
-
-cols = '''\
-4 2 8
-4 2 2 1 4
-8 3 4 2
-4 5 2 4 1
-4 5 2 6
-4 4 8
-4 4 5 2
-5 6 3 5
-2 1 1 4 4
-4 3 9
-3 4 1 1 2
-1 3 1 1 1 1
-5 3 1 1 2
-12 4 2
-5 7 2
-3 6 1 4 1
-2 6 1 5 1
-3 3 1 4
-2 3 2 4 3
-4 4 1 1
-7 4 1 2 1
-7 6 1 3 1 2
-9 4 7 1 1
-4 2 3 10
-3 3 4 1
-1 2 3 4
-1 6 2 3 1
-3 6 9
-5 8 7 1
-3 4 2 8 1
-'''
 
 CODE_BLACK = 'H'
 CODE_UNKNOWN = '.'
@@ -91,8 +28,8 @@ def text_to_numbers(one_txt):
     return list(map(line_to_numbers, all_lines))
 
 
-rows = text_to_numbers(rows)
-cols = text_to_numbers(cols)
+rows = text_to_numbers(fx_area.rows)
+cols = text_to_numbers(fx_area.cols)
 
 ZnakEscape = chr(27)
 
