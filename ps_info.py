368ef450d7c4dcb1ac9942a009564ae923a9914f
Piotr Kasprzyk <ciri@ciri.pl>
2023-06-16 09:13:20 +0200

    Move prepare_empty_data to PositionVault

--- ps_info.py
+++ ps_info.py
@@ -3,6 +3,8 @@
 
 import unittest
 
+import lb_cnst
+
 
 class PositionVault:
     def __init__(self, row_count, col_count):
@@ -11,6 +13,18 @@ class PositionVault:
         '''
         self.row_cnt = row_count
         self.col_cnt = col_count
+
+    def prepare_empty_data(self):
+        '''
+        PositionVault:
+        '''
+        lcl_tbl = []
+        for row_nr in range(self.row_cnt):
+            line_ls = []
+            for col_nr in range(self.col_cnt):
+                line_ls.append(lb_cnst.CODE_UNKNOWN)
+            lcl_tbl.append(line_ls)
+        return lcl_tbl
 
 
 class TestPositionDetails(unittest.TestCase):
@@ -21,3 +35,10 @@ class TestPositionDetails(unittest.TestCase):
         obj = PositionVault(row_count=5, col_count=5)
         self.assertEqual(obj.row_cnt, 5)
         self.assertEqual(obj.col_cnt, 5)
+
+    def test_initial_emtiness(self):
+        '''
+        TestPositionDetails:
+        '''
+        obj = PositionVault(row_count=2, col_count=2)
+        self.assertEqual(obj.prepare_empty_data(), [['.', '.'], ['.', '.']])
